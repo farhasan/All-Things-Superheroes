ba0135b621dc925786de0847d76b80c3fc3abfe7
Farhan Hasan <farhanhasan@Farhans-MBP.fios-router.home>
2017-11-10 10:44:53 -0500

    connection not working

--- src/allThingsSuperHeroes/db.js
+++ src/allThingsSuperHeroes/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const config = require('./config');
 
 /*
 const Comic = new mongoose.Schema({ //comic specific issue
@@ -48,15 +49,8 @@ const CustomHero = new mongoose.Schema({ //customhero made by the user
 if (process.env.NODE_ENV === 'PRODUCTION') {
     // if we're in PRODUCTION mode, then read the configration from a file
     // use blocking file io to do this...
-    const fs = require('fs');
-    const path = require('path');
-    const fn = path.join(__dirname, 'config.json');
-    const data = fs.readFileSync(fn);
-
-    // our configuration file will be in json, so parse it and set the
-    // conenction string appropriately!
-    const conf = JSON.parse(data);
-    let dbconf = conf.dbconf;
+    const connection = config.connection;
+    var dbconf = connection.dbconf;
 } else {
     // if we're not in PRODUCTION mode, then use
     dbconf = 'mongodb://localhost/example_db';
